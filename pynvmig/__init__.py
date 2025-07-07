cc5f6c3ce6c357b6e7842fd5bae24ebad01a41fa
Zhiwei Chu <chi32@pnw.edu>
2025-07-07 11:09:21 -0400

    bump patch version number to 2

--- pynvmig/__init__.py
+++ pynvmig/__init__.py
@@ -3,7 +3,7 @@ from .mig_manager import MIGConfigManager
 
 _MAJOR = 0  # Major version number
 _MINOR = 1  # Minor version number
-_PATCH = 1  # Patch version number
+_PATCH = 2  # Patch version number
 
 __version__ = f"{_MAJOR}.{_MINOR}.{_PATCH}"
 
